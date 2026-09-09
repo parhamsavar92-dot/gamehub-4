1250bb93d82e7f9bcb74aa535d4861f544eb75e7
parhamsavar92-dot <parham.savar92@gmail.com>
2026-09-09 11:10:45 +0330

    Update server.js

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ app.get('/api/live', (req,res)=>{
   req.on('close', ()=>{ clearInterval(ping); sseClients.delete(res); });
 });
 app.get('/*splat',(req,res)=>res.sendFile(path.join(ROOT,'index.html')));
-app.get('*',(req,res)=>res.sendFile(path.join(ROOT,'index.html')));
+
 const port=Number(process.env.PORT||3000);
 app.listen(port,()=>console.log(`GameHub server listening on http://localhost:${port}`));
 
